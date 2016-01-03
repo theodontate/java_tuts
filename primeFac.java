ad2fbbb65c31f7eaa8ef67eb484b877f2cf85c6c
Naveen <princeofdarkness4444@gmail.com>
2016-01-04 01:27:15 +0300

    Largest Prime Factor implemented.

--- primeFac.java
+++ primeFac.java
@@ -5,27 +5,38 @@ import java.util.ArrayList;
  */
 public class primeFac {
     public static void main(String[] args) {
-        ArrayList<Long> primeFactors = new ArrayList<>();
+        ArrayList<Integer> primeFactors = new ArrayList<>();
         long theNumber = 600851475143L;
-        long bound = theNumber/2L;
+        long bound = (long) Math.sqrt(theNumber);
         long largestPrimeFactor = 1;
+        int numberOfFactors = 0;
 
         System.out.println(theNumber);
         System.out.println("bound : " + bound);
 
-//        for (long i = 1L; i < bound; i++) {
-//            if ((i%2) == 0) {
-//                primeFactors.add(i);
-//                System.out.println(i);
-//            }
-//        }
-//
-//        for (Long factor : primeFactors) {
-//            if (largestPrimeFactor < factor) {
-//                largestPrimeFactor = factor;
-//                System.out.println(largestPrimeFactor);
-//            }
-//        }
+        for (int i = 2; i < (int) bound; i++) {
+            if ((theNumber%i) == 0) {
+                for (int j = 2; j < i/2; j++) {
+                    if((i%j) == 0) {
+                        numberOfFactors++;
+                    }
+                }
+
+                if (numberOfFactors < 2) {
+                    primeFactors.add(i);
+                    numberOfFactors = 0;
+                } else {
+                    numberOfFactors = 0;
+                }
+
+            }
+        }
+
+        for (Integer factor : primeFactors) {
+            if (largestPrimeFactor < factor) {
+                largestPrimeFactor = factor;
+            }
+        }
 
         System.out.println("Largest Prime Factor : " + largestPrimeFactor);
     }
